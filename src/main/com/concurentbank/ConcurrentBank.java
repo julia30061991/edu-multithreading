e30bc46960ef51b9de1e5f3d12af0ac86d937655
julia30061991 <chursina1991@yandex.ru>
2024-02-07 20:53:01 +0300

    changed concurrent bank

--- src/main/com/concurentbank/ConcurrentBank.java
+++ src/main/com/concurentbank/ConcurrentBank.java
@@ -5,9 +5,10 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class ConcurrentBank {
-    private static final List <BankAccount> accountList = new ArrayList<>();
+    private static final List<BankAccount> accountList = new ArrayList<>();
 
-    public ConcurrentBank() {}
+    public ConcurrentBank() {
+    }
 
     public List<BankAccount> getAccountList() {
         return accountList;
@@ -20,18 +21,20 @@ public class ConcurrentBank {
         return account;
     }
 
-    public void transfer(BankAccount account1, BankAccount account2, AtomicLong money) {
-            if (account1.getBalance().longValue() >= money.longValue()) {
-                account1.withdraw(account1, money.longValue());
-                account2.deposit(account2, money.longValue());
-            } else {
-                System.out.println("На счету недостаточно средств для перевода");
-            }
+    public synchronized void transfer(BankAccount account1, BankAccount account2, AtomicLong money) {
+        if (account1.getBalance().longValue() >= money.longValue()) {
+            account1.withdraw(account1, money.longValue());
+            System.out.println("Списана сумма: " + money);
+            account2.deposit(account2, money.longValue());
+            System.out.println("Зачислена сумма: " + money);
+        } else {
+            System.out.println("На счету недостаточно средств для перевода");
+        }
     }
 
     public AtomicLong getTotalBalance() {
         AtomicLong result = new AtomicLong(0);
-        for (BankAccount account: accountList) {
+        for (BankAccount account : accountList) {
             AtomicLong money = account.getBalance();
             result.addAndGet(money.longValue());
         }
